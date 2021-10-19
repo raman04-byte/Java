b2e7b8a1481a98a3f3e136fc2c0e98d80143e107
raman04-byte <2020pgicsraman65@poornima.org>
2021-10-19 18:28:17 +0530

    Added some new lines

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("Hello world");
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
     }
 }
