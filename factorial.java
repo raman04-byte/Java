6b77772a0579553970e9aa004584995e93e31645
raman04-byte <2020pgicsraman65@poornima.org>
2021-10-20 20:11:26 +0530

    Added a new factorial file

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        System.out.println("Enter a number you want to calculate factorial");
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int result = 1;
+        
+        for (int i = a; i >= 1; i--) {
+            result = result*i;
+        }
+        System.out.println(result);
+    }
+}
