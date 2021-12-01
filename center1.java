3f8850ef07e497228d403f4ddd1fe30569966e3d
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-01 18:55:30 +0530

    added some format

--- center1.java
+++ center1.java
@@ -1,18 +1,16 @@
 import java.util.Random;
-public class center1 
-{
- public static void main(String[] args) 
- {
-    Random ran = new Random();
-    int freq[] = new int[7];
-    for (int roll_no = 0; roll_no < 1000; roll_no++) 
+
+public class center1 {
+    public static void main(String[] args) 
     {
-        ++freq[1+ran.nextInt(6)];    
+        Random ran = new Random();
+        int freq[] = new int[7];
+        for (int roll_no = 0; roll_no < 1000; roll_no++) {
+            ++freq[1 + ran.nextInt(6)];
+        }
+        System.out.println("face\tfrequency");
+        for (int face = 0; face < freq.length; face++) {
+            System.out.println(face + "\t" + freq[face]);
+        }
     }
-    System.out.println("face\tfrequency");
-    for (int face = 0; face < freq.length; face++) 
-    {
-        System.out.println(face+"\t"+freq[face]);    
-    }
- }   
 }
