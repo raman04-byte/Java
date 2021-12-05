13106b9b7035242efa11675ecab4d7d7b2ddeb22
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-05 16:44:04 +0530

    Time

--- center.java
+++ center.java
@@ -1,10 +1,11 @@
 //import java.util.Scanner;
 //import java.util.Random;
-public class center 
-{
-    public static void main(String[] args) 
-    {
-        
+public class center {
+    public static void main(String[] args) {
+        tuna tunaObj = new tuna();
+        System.out.println(tunaObj.toMilitary());
+        tunaObj.setTime(6, 60, 24);
+        System.out.println(tunaObj.toMilitary());
     }
-    
+
 }
