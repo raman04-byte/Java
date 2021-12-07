7fc1411841989713ed34b9c9b92223450ad1f219
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-07 19:49:09 +0530

    Chenges

--- test.java
+++ test.java
@@ -6,30 +6,9 @@ import java.util.*;
 // import java.util.concurrent.*;
 // import java.util.regex.*;
 
-public class test {
-
-    
-
-    private static final Scanner scanner = new Scanner(System.in);
-
+public class test 
+{
     public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        scanner.close();
-      if (N%2==1){
-          System.out.println("Weird");
-      }
-      else if(N%2==0 && (N>=2 && N<=5)){
-          System.out.println("Not Weird");
-      }
-      else if(N%2==0 && (N>5 && N<=20)){
-          System.out.println("Weird");
-      }
-      else if(N%2==0 && (N>20 && N<=100)){
-          System.out.println("Not Weird");
-      }
-
-
+        System.out.println("Done this program and doing coding in Hacker rank");
     }
 }
