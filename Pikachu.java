6678f63078f7dfbc96eec3ae674f2d6750b036c8
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-10 20:18:59 +0530

    Added some files

--- Pikachu.java
+++ Pikachu.java
@@ -1,16 +1,4 @@
-import java.util.Scanner;
-public class Pikachu {
-    public static void main(String[] args) 
-    {
-        try (// System.out.println("The code is running ");
-        Scanner input = new Scanner(System.in))
-        {
-            System.out.println("Enter name here : ");
-            String temp = input.nextLine();
-            nitin obj = new nitin();
-            obj.setName(temp);
-            obj.name();
-        }
-    }
+public class Pikachu 
+{
     
 }
